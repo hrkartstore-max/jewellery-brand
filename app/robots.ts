ee44116cc9c65d0c13ad7ce6bc78d998b01d31f5
hrkartstore-max <hrkartstore@gmail.com>
2026-09-23 21:23:30 +0530

    Upgrade HEPRA Jewellery app/robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,8 @@
-import type {MetadataRoute} from 'next';export default function robots():MetadataRoute.Robots{return{rules:{userAgent:'*',allow:'/'},sitemap:'https://aurelia-jewellery.vercel.app/sitemap.xml'}}
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: { userAgent: '*', allow: '/' },
+    sitemap: 'https://jewellery-brand-4ajl-p6uueg39h-hepra1.vercel.app/sitemap.xml',
+  };
+}
